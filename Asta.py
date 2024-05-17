6b931192f4f8860513065e7396f3b6cf388dfe2d
HunterStile <you@example.com>
2024-05-17 03:21:33 +0200

    Bot che esegue L'asta

---
+++ Asta.py
@@ -0,0 +1,34 @@
+from funzioni.bidoo_functions import *
+import pandas as pd
+import pyautogui
+
+
+#ACCESSO
+username = 'caprettainf'
+password = 'Bolleblu1'
+
+#variabili
+xpath_tamplate = '//*[@id="{}"]/div[3]/section[1]/div/section[1]/a'
+file_bidoo = 'account_bidoo.csv'
+
+
+## >main<
+print("Benvenuti nel bot di bidoo by HunterStile!")
+
+
+driver = configurazione_browser()
+accesso_bidoo(driver,username,password)
+asta = input("Seleziona l'asta(inseire DA come iniziale): ")
+xpath = xpath_tamplate.format(asta)
+Last_second(driver,xpath)
+
+
+
+
+
+
+
+
+
+
+
